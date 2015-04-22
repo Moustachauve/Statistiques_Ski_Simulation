4691f475d1d7df4db042b14e0abe3d0699d2d223
IceSentry <c.giguere42@gmail.com>
2015-04-22 10:45:41 -0400

    Add Sortie properties

--- statistiques ski/statistiques ski/Models/Sortie.cs
+++ statistiques ski/statistiques ski/Models/Sortie.cs
@@ -1,11 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace statistiques_ski.Models
 {
-    public class Sortie
-    {
-    }
+	public class Sortie
+	{
+		public int sortieID { get; set; }
+
+		[Display(Name="Nombre de descente")]
+		public int nbDescente { get; set; }
+
+		[Display(Name = "Nombre de pied verticaux")]
+		public int nbPiedVert { get; set; }
+
+		[DataType(DataType.Date)]
+		[Display(Name = "Date de sortie")]
+		public DateTime date { get; set; }
+
+		[ForeignKey("CentreDeSki")]
+		public int centreDeSkiID { get; set; }
+		public virtual CentreDeSki centreDeSki { get; set; }
+
+		[ForeignKey("Skieur")]
+		public int skieurID { get; set; }
+		public virtual Skieur skieur { get; set; }
+
+		[ForeignKey("Saison")]
+		public int saisonID { get; set; }
+		public virtual Saison saison { get; set; }
+	}
 }
