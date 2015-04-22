cdad9e07f75f11561d5380e80862883cc68b6530
IceSentry <c.giguere42@gmail.com>
2015-04-22 16:37:30 -0400

    Finish Statistiques Controller and View

--- statistiques ski/statistiques ski/Controllers/StatistiquesController.cs
+++ statistiques ski/statistiques ski/Controllers/StatistiquesController.cs
@@ -8,17 +8,47 @@ using System.Web.Mvc;
 
 namespace statistiques_ski.Controllers
 {
-    public class StatistiquesController : Controller
-    {
+	public class StatistiquesController : Controller
+	{
 		private UnitOfWork uow = new UnitOfWork();
-        // GET: Statistique
-        public ActionResult Index()
-        {
-			ViewBag.sorties = uow.SortieRepository.Get();
-			ViewBag.centreDeSkis = uow.CentreDeSkiRepository.Get();
-			ViewBag.saisons = uow.SaisonRepository.Get();
 
-            return View();
-        }
-    }
+		public ActionResult Index(FormCollection form)
+		{
+			if (form != null)
+			{
+				var filterBy = form.GetValue("filterBy");
+
+				if (filterBy != null)
+				{
+					int temp = (int)filterBy.ConvertTo(typeof(int));
+					if (temp != 0)
+					{
+						ViewBag.centreDeSkis = uow.CentreDeSkiRepository.Get(filter: x => x.RegionID == temp && x.Region.SkieurID == uow.CurrentUserID);
+						ViewBag.RegionID = temp;
+					}
+					else
+					{
+						ViewBag.centreDeSkis = uow.CentreDeSkiRepository.GetForSkieur(uow.CurrentUserID);
+						ViewBag.RegionID = temp;
+					}
+				}
+				else
+				{
+					ViewBag.centreDeSkis = uow.CentreDeSkiRepository.GetForSkieur(uow.CurrentUserID);
+					ViewBag.RegionID = 0;
+				}
+			}
+			else
+			{
+				ViewBag.centreDeSkis = uow.CentreDeSkiRepository.GetForSkieur(uow.CurrentUserID);
+				ViewBag.RegionID = 0;
+			}
+
+			ViewBag.sorties = uow.SortieRepository.GetForSkieur(uow.CurrentUserID);
+			ViewBag.saisons = uow.SaisonRepository.GetForSkieur(uow.CurrentUserID);
+			ViewBag.regions = uow.RegionRepository.GetForSkieur(uow.CurrentUserID);
+
+			return View();
+		}
+	}
 }
