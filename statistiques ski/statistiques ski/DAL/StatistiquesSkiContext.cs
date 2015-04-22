fa261affe073407f1c9288228029cb04c5985f8e
Christophe Gagnier <admin@cgagnier.ca>
2015-04-22 09:02:15 -0400

    Add Entity Framework Take 2

---
+++ statistiques ski/statistiques ski/DAL/StatistiquesSkiContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace statistiques_ski.DAL
+{
+    public class Statistiques_SkiContext : DbContext
+    {
+        public Statistiques_SkiContext() : base() {}
+    }
+}
