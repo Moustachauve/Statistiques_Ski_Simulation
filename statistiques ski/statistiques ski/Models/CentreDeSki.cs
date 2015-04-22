fc89db4302127aefe4e6184a4930fee1484f4a51
Christophe Gagnier <admin@cgagnier.ca>
2015-04-22 10:33:17 -0400

    Add CentreDeSki properties

--- statistiques ski/statistiques ski/Models/CentreDeSki.cs
+++ statistiques ski/statistiques ski/Models/CentreDeSki.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,23 @@ namespace statistiques_ski.Models
 {
     public class CentreDeSki
     {
+
+		public int CentreDeSkiID { get; set; }
+
+		public string Nom { get; set; }
+
+		public string Adresse { get; set; }
+
+		public int NbPistes { get; set; }
+
+		public float Altitude { get; set; }
+
+
+
+		[ForeignKey("Region")]
+		public int RegionID { get; set; }
+		public virtual Region Region { get; set; }
+
+		public virtual ICollection<Sortie> Sorties { get; set; }
     }
 }
