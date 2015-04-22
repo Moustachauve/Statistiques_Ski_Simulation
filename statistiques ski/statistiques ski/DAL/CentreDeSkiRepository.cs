de5176fc4d53ef7af06341af59892bb14a51b198
Christophe Gagnier <admin@cgagnier.ca>
2015-04-22 11:43:02 -0400

    Add CentreDeSkiRepository and SkieurRepository

--- statistiques ski/statistiques ski/DAL/CentreDeSkiRepository.cs
+++ statistiques ski/statistiques ski/DAL/CentreDeSkiRepository.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using statistiques_ski.Models;
 
 namespace statistiques_ski.DAL
 {
-    public class CentreDeSkiRepository
+    public class CentreDeSkiRepository : Repository<CentreDeSki>
     {
+		public CentreDeSkiRepository(Statistiques_SkiContext context) : base(context)
+        {}
+
+		public IEnumerable<CentreDeSki> Get()
+		{
+			return Get(includeProperties: "Region,Sortie");
+		}
     }
 }
--- statistiques ski/statistiques ski/DAL/SkieurRepository.cs
+++ statistiques ski/statistiques ski/DAL/SkieurRepository.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using statistiques_ski.Models;
 
 namespace statistiques_ski.DAL
 {
-    public class SkieurRepository
+    public class SkieurRepository : Repository<Skieur>
     {
+
+		public SkieurRepository(Statistiques_SkiContext context): base(context)
+        {}
+
+		public IEnumerable<Skieur> Get()
+		{
+			return Get(includeProperties: "Saison,Sortie,Saison,Region,CentreDeSki");
+		}
     }
 }
