ae8e1bf4ff26c30be4224d23e4c15ea791bf4cdb
Christophe Gagnier <admin@cgagnier.ca>
2015-04-22 10:52:00 -0400

    Update Sortie properties (Uppercase)

--- statistiques ski/statistiques ski/Models/Sortie.cs
+++ statistiques ski/statistiques ski/Models/Sortie.cs
@@ -9,28 +9,28 @@ namespace statistiques_ski.Models
 {
 	public class Sortie
 	{
-		public int sortieID { get; set; }
+		public int SortieID { get; set; }
 
 		[Display(Name="Nombre de descente")]
-		public int nbDescente { get; set; }
+		public int NbDescente { get; set; }
 
 		[Display(Name = "Nombre de pied verticaux")]
-		public int nbPiedVert { get; set; }
+		public int NbPiedVert { get; set; }
 
 		[DataType(DataType.Date)]
 		[Display(Name = "Date de sortie")]
-		public DateTime date { get; set; }
+		public DateTime Date { get; set; }
 
 		[ForeignKey("CentreDeSki")]
-		public int centreDeSkiID { get; set; }
-		public virtual CentreDeSki centreDeSki { get; set; }
+		public int CentreDeSkiID { get; set; }
+		public virtual CentreDeSki CentreDeSki { get; set; }
 
 		[ForeignKey("Skieur")]
-		public int skieurID { get; set; }
-		public virtual Skieur skieur { get; set; }
+		public int SkieurID { get; set; }
+		public virtual Skieur Skieur { get; set; }
 
 		[ForeignKey("Saison")]
-		public int saisonID { get; set; }
-		public virtual Saison saison { get; set; }
+		public int SaisonID { get; set; }
+		public virtual Saison Saison { get; set; }
 	}
 }
