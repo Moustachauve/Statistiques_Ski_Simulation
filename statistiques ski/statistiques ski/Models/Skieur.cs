c766414b38fcca93f581940f59a496397427c7ac
Christophe Gagnier <admin@cgagnier.ca>
2015-04-22 10:38:34 -0400

    Add Skieur properties

--- statistiques ski/statistiques ski/Models/Skieur.cs
+++ statistiques ski/statistiques ski/Models/Skieur.cs
@@ -7,5 +7,13 @@ namespace statistiques_ski.Models
 {
     public class Skieur
     {
+		public int SkieurID { get; set; }
+		public string Nom { get; set; }
+		public string Prenom { get; set; }
+
+		public virtual ICollection<Saison> Saisons { get; set; }
+		public virtual ICollection<Sortie> Sorties { get; set; }
+		public virtual ICollection<CentreDeSki> CentreDeSkis { get; set; }
+		public virtual ICollection<Region> Regions { get; set; }
     }
 }
